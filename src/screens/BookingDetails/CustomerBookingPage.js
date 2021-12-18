e2c542ca71a025c802a7ca724a1b5392b6c12d54
Samra Hanif <samra.Hanif@systemsltd.com>
2021-12-18 19:40:28 +0500

    booking details page ui implemented along with the form

--- src/screens/BookingDetails/CustomerBookingPage.js
+++ src/screens/BookingDetails/CustomerBookingPage.js
@@ -1,83 +1,186 @@
 import React from "react";
-import { Button, TextInput, View,StatusBar,ImageBackground,StyleSheet,Text } from "react-native";
+import { Button, TextInput, View, StatusBar, ImageBackground, StyleSheet, Text } from "react-native";
 import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
 
-import { TouchableHighlight,TouchableOpacity } from "react-native-gesture-handler";
+import { TouchableHighlight, TouchableOpacity } from "react-native-gesture-handler";
 // import { TextField } from "react-native-material-textfield";
 import { TextField } from '../../components/customComponents/customComponents'
- 
+
 const validationSchema = Yup.object().shape({
   name: Yup.string()
-  .min(2, 'Too Short!')
-  .max(50, 'Too Long!')
-  .required('Required'),
+    .min(2, 'Name must be atleast 6 characters long')
+    .max(80, 'ame must be atmost 80 characters long')
+    .required('Required'),
 
-email: Yup.string().email('Enter a valid EMAIL (abc@abc.com)').required('Required').min(50,'Email must be atmost 50 characters long'),
-cnic: Yup.string()
-  .min(2, 'Too Short!')
-  .max(50, 'Too Long!')
-  .required('Required'),
+  email: Yup.string().email('Enter a valid EMAIL (abc@abc.com)').required('Required').max(50, 'Email must be atmost 50 characters long'),
+  cnic: Yup.string()
+    .min(13, 'CNIC must be of 13 characters long')
+    .max(13, 'CNIC must be of 13 characters long')
+    .required('Required'),
+  mobileNumber: Yup.string()
+    .min(11, 'Mobile Number should be in format 03xxxxxxxxx')
+    .max(11, 'Mobile Number should be in format 03xxxxxxxxx')
+    .required('Required'),
 });
- 
-const CustomerBookingPage =  (props) =>
- {
-  return(
-  <View style={styles.container}>
-  <StatusBar barStyle="light-content" backgroundColor="rgba(142,7,27,1)" />
-  <ImageBackground style={styles.container}
-    source={require("../../assets/images/Gradient_MI39RPu.png")}
-  >
-    <View style={styles.titleWrapper}>
-                    <Text style={styles.title}>Enter Booking Details</Text>
-    </View>
-<Formik
-       initialValues={{
-        name: '',
-        email: '',
-        cnic: '',
-        phoneNumber:'',       
-       }}
-       validationSchema={validationSchema}
-       onSubmit={(values,errors) => {
-         // same shape as initial values
-         console.log(errors)
-         console.log(values);
-       }}
-     >
+
+const saveData = async (data) => {
+  console.log('in save data')
+  setErrortext(null)
+  try {
+      setIsLoading(true);
+      const response = await axios(
+          configurationObject,
     
-     {({ handleChange, handleBlur, handleSubmit, values,errors, touched, isValidating }) => (
-      <View>       
-        <TextField
-         placeholder="Username/Email" style={styles.labelText}
-         keyboardType='email-address'
-         mode="outlined"
-         placeholderTextColor="#800000"
-         nameOfIcon="inbox"
-         maxLength={50}
-          onChangeText={handleChange('email')}
-          onBlur={handleBlur('email')}
-          value={values.email}
-          placeholder="Email"
-          error={[errors.email]}
-        />
-        <TouchableOpacity
-                        onPress={handleSubmit}
-                        style={styles.submitButtonWrapper}
-                    >
-                        <Text style={styles.submitButtonText}>Send Book Request</Text>
-                    </TouchableOpacity>
-      </View>
-    )}
-      
-     </Formik>
-   
-</ImageBackground>
-</View>
-  )}
+      );
+      alert(response.ResponseCode)
+      console.log(response)
+      if (response.data.ResponseCode === "00") {
+          setIsLoading(false);
+          setErrortext({text:'Success',styles:ToastStyles.success})
+          Snackbar.show({
+              text: response.data.ResponseDesc,
+              duration: Snackbar.LENGTH_LONG,
+            });
+          //   AsyncStorage.setItem('user_id', response.data.Username);
+          //   console.log(response.data.Username);
+          
+          return;
+      } else {
+          setIsLoading(false);
+          setErrortext({text:response.data.ResponseDesc,styles:ToastStyles.error})
+          Snackbar.show({
+              text: response.data.ResponseDesc,
+              duration: Snackbar.LENGTH_INDEFINITE,
+              action: {
+                text: 'OK',
+                textColor: 'white',
+                onPress: () => { /* Do something. */ },
+              },
+            });
+          // throw new Error("Failed to fetch users");
+      }
+  } catch (error) {
+      setIsLoading(false);
+      Snackbar.show({
+          text: 'Something Went Wrong',
+          duration: Snackbar.LENGTH_INDEFINITE,
+          action: {
+            text: 'OK',
+            textColor: 'white',
+            onPress: () => { /* Do something. */ },
+          },
+        });
+
+  }
+};
+
+const CustomerBookingPage = (props) => {
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle="light-content" backgroundColor="rgba(142,7,27,1)" />
+      <ImageBackground style={styles.container}
+        source={require("../../assets/images/Gradient_MI39RPu.png")}
+      >
+        <View style={styles.titleWrapper}>
+          <Text style={styles.title}>Enter Booking Details</Text>
+        </View>
+        <Formik
+          initialValues={{
+            name: '',
+            email: '',
+            cnic: '',
+            mobileNumber: '',
+          }}
+          validationSchema={validationSchema}
+          onSubmit={(values, errors) => {
+            // same shape as initial values
+            console.log(errors)
+            console.log(values);
+          }}
+        >
+
+          {({ handleChange, handleBlur, handleSubmit, values, errors, touched, isValidating }) => (
+            <View>
+              <TextField
+                placeholder="Name" style={styles.labelText}
+                keyboardType='default'
+                mode="outlined"
+                placeholderTextColor="#800000"
+                nameOfIcon="user"
+                maxLength={80}
+                onChangeText={handleChange('name')}
+                onBlur={handleBlur('name')}
+                value={values.name}
+                error={[errors.name]}
+              />
+              <TextField
+                placeholder="Email" style={styles.labelText}
+                keyboardType='email-address'
+                mode="outlined"
+                placeholderTextColor="#800000"
+                nameOfIcon="envelope"
+                maxLength={50}
+                onChangeText={handleChange('email')}
+                onBlur={handleBlur('email')}
+                value={values.email}
+                error={[errors.email]}
+              />
+              <TextField
+                placeholder="CNIC" style={styles.labelText}
+                keyboardType='number'
+                mode="outlined"
+                placeholderTextColor="#800000"
+                nameOfIcon="credit-card"
+                maxLength={13}
+                onChangeText={handleChange('cnic')}
+                onBlur={handleBlur('cnic')}
+                value={values.cnic}
+                error={[errors.cnic]}
+              />
+              <TextField
+                placeholder="Mobile Number" style={styles.labelText}
+                keyboardType='phone-pad'
+                mode="outlined"
+                placeholderTextColor="#800000"
+                nameOfIcon="bell"
+                maxLength={11}
+                onChangeText={handleChange('mobileNumber')}
+                onBlur={handleBlur('mobileNumber')}
+                value={values.mobileNumber}
+                error={[errors.mobileNumber]}
+              />
+
+              <View style={styles.eventDetails}>
+                <View style={styles.eventChilds}>
+                  <Text style={styles.eventChilds.content.viewTypeLeft}>Event Date:</Text>
+                  <Text style={styles.eventChilds.content.viewTypeLeft}>Event Time:</Text>
+                  <Text style={styles.eventChilds.content.viewTypeLeft}>Advance Payment:</Text>
+                </View>
+                <View style={styles.eventChilds}>
+                <Text style={styles.eventChilds.content.viewTypeRight}>14-12-2021</Text>
+                  <Text style={styles.eventChilds.content.viewTypeRight}>7PM - 11 PM</Text>
+                  <Text style={styles.eventChilds.content.viewTypeRight}>30,000 PKR</Text>
+                </View>
+              </View>
+              <TouchableOpacity
+                onPress={handleSubmit}
+                style={styles.submitButtonWrapper}
+              >
+                <Text style={styles.submitButtonText}>Send Book Request</Text>
+              </TouchableOpacity>
+            </View>
+          )}
+
+        </Formik>
+
+      </ImageBackground>
+    </View>
+  )
+}
 
 
-  
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -109,13 +212,36 @@ const styles = StyleSheet.create({
     marginRight: 20,
     marginLeft: 20,
     marginTop: 14,
-    marginBottom:14
+    marginBottom: 14
   },
   submitButtonText: {
     color: "rgba(255,255,255,1)",
     textAlign: "center",
     fontSize: 20,
     alignSelf: "center"
+  },
+  eventDetails:{
+    marginRight: 20,
+    marginLeft: 20,
+    marginTop: 14,
+    marginBottom: 14,
+    flexDirection:'row',
+    
+  },
+  eventChilds:{  
+    flex:6,
+    content:{
+      viewTypeLeft:{
+      color:'white',
+        alignSelf:'flex-start',
+        alignContent:'space-around'
+      },
+      viewTypeRight:{
+      color:'white',
+        alignSelf:'flex-end',
+        alignContent:'space-around'
+      }
+    }
   }
 
 })
