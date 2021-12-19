9765a41e55fa8364c58349ce22e7919afbf2005f
Muzna-Folio <78356421+Muzna-Folio@users.noreply.github.com>
2021-12-19 23:59:20 +0500

    commiting server updating link

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'http://52e2-202-5-147-189.ngrok.io/HallFromHome/';
+export const BASE_URL = 'http://04ec-202-5-153-208.ngrok.io/HallFromHome/';
 // export const BASE_URL = 'https://jsonplaceholder.typicode.com/';
 
