97757b49704c58482ebd829ff2811c8218428d13
Samra Hanif <samra.Hanif@systemsltd.com>
2021-12-20 17:49:08 +0500

    base url added

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'http://8291-202-5-153-208.ngrok.io/HallFromHome/';
+export const BASE_URL = 'http://dddb-37-111-136-92.ngrok.io/HallFromHome/';
 // export const BASE_URL = 'https://jsonplaceholder.typicode.com/';
 
