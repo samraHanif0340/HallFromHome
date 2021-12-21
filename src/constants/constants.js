89e3a5cdb73c593083c9a7adcb72647516f85130
Muzna-Folio <78356421+Muzna-Folio@users.noreply.github.com>
2021-12-21 23:09:42 +0500

    resolving conflicts in constant.js file

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'http://dddb-37-111-136-92.ngrok.io/HallFromHome/';
+export const BASE_URL = 'http://74cf-37-111-130-131.ngrok.io/HallFromHome/';
 // export const BASE_URL = 'https://jsonplaceholder.typicode.com/';
 
