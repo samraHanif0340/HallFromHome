c394bec358ce7a2aa1391238799e8d9c465503ac
Muzna-Folio <78356421+Muzna-Folio@users.noreply.github.com>
2021-12-23 22:36:51 +0500

    resolving conflicts

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-export const BASE_URL = 'http://74cf-37-111-130-131.ngrok.io/HallFromHome/';
-=======
 export const BASE_URL = 'http://localhost:58076/HallFromHome/';
->>>>>>> 74aaabf5da404de372d2feacfc88ebf635db7b1b
 // export const BASE_URL = 'https://jsonplaceholder.typicode.com/';
 
