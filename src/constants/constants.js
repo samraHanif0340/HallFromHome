1aa5a87b5d5b3d61c10ffa1c642067ff81d2b7c9
Samra Hanif <samrahanif@Samras-MacBook-Pro.local>
2022-05-08 17:40:31 +0500

    taking changes

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,6 @@
-export const BASE_URL = 'http://localhost:58076/HallFromHome/';
+// export const BASE_URL = 'http://localhost:58076/HallFromHome/';
+export const BASE_URL = 'http://humayunmukhtar1-001-site1.itempurl.com/HallFromHome/';
+
 export const ERROR_MESSAGES = {
     SOMETHING_WENT_WRONG: 'Something went wrong. Please contact Administrator'
 }
